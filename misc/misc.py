01abb3264df4e73973a77d6204e6fc102e7ecc01
Jernej Krempuš <jkrempus@gmail.com>
2018-07-01 23:35:30 +0200

    Added misc/misc.py.

---
+++ misc/misc.py
@@ -0,0 +1,24 @@
+from numpy import *
+import sys
+from array_ipc import received, Received, listen
+
+def get_real(a, vsz=None):
+    if vsz is None:
+        return a[0 : len(a) / 2]
+    else:
+        return a.reshape(-1, vsz)[0 : -1 : 2].flatten()
+
+def get_imag(a, vsz=None):
+    if vsz is None:
+        return a[len(a) / 2:-1]
+    else:
+        return a.reshape(-1, vsz)[1 : -1 : 2].flatten()
+
+def print_array(a, num_columns = 4, precision = 3):
+    width = precision + 6;
+    fmt = "{{:{}.{}g}} ".format(width, precision)
+    for i, e in enumerate(a):
+        if i != 0 and i % num_columns == 0: sys.stdout.write("\n"); 
+        sys.stdout.write(fmt.format(e))
+
+    sys.stdout.write("\n");
