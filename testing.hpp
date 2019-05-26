781facf4a68ec6e198654c7d31e0e29b7426dc87
Jernej Krempuš <jkrempus@gmail.com>
2019-05-26 20:17:17 +0200

    testing: Initialized Options members

--- testing.hpp
+++ testing.hpp
@@ -6,18 +6,18 @@ struct SizeRange
   Int end;
 };
 
-struct SimdImpl { Int val; };
+struct SimdImpl { Int val = 0; };
 
 struct Options
 {
-  bool is_double;
-  bool is_bench;
-  bool is_real;
-  bool is_inverse;
+  bool is_double = false;
+  bool is_bench = false;
+  bool is_real = false;
+  bool is_inverse = false;
   std::optional<double> precision;
-  double num_ops;
+  double num_ops = 0.0;
   SimdImpl simd_impl;
-  ptrdiff_t misaligned_by;
+  ptrdiff_t misaligned_by = 0;
   std::string implementation;
   std::vector<SizeRange> size;
 };
